fbabdc1d668e536331f49baa2220f4dd645c469f
Sarah Griffin <sfgriffin@hotmail.com>
2021-06-21 15:23:04 -0500

    Initial upload from Module 10 exercises

---
+++ app.py
@@ -0,0 +1,9 @@
+# Module 10 exercises - Section 10.5.1
+# Creating the Flask app for the Mission-to-Mars unit
+
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import Module10_scraping   # scraping code
+
+
+
